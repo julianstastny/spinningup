7156947ebfd6fcb49e5df138864ff6c04d2758de
Julian Stastny <julianstastny@gmail.com>
2021-03-12 13:55:48 +0100

    Fixed bug in environment

--- spinup/algos/pytorch/td3/curriculum_env.py
+++ spinup/algos/pytorch/td3/curriculum_env.py
@@ -7,35 +7,29 @@ import numpy as np
 class CurriculumEnv(HockeyEnv):
   
   def __init__(self, mode=None):
-    self.opponent = None
+    self.opponent = BasicOpponent(weak=True)
     self.episode = 0
     self.curriculum = {
-        "defense+shooting": 1000,
-        "weak": 1500
+        "defense+shooting": 1000, # Until episode 1000
+        "play vs weak": 2000 # Until episode 2000. After that, play vs strong
     }
     super().__init__(mode=1, keep_mode=True)
     # linear force in (x,y)-direction, torque, and shooting
-    # self.action_space = spaces.Box(-1, +1, (4,), dtype=np.float32)
+    self.action_space = spaces.Box(-1, +1, (4,), dtype=np.float32)
 
 
   def reset(self, one_starting=None):
     self.episode += 1
     if self.episode < self.curriculum["defense+shooting"]:
-        print("training defense and shooting")
         if self.episode % 2 == 0:
             return super().reset(one_starting, mode=1)
         return super().reset(mode=2)
-    elif self.episode < self.curriculum["weak"]:
-        self.opponent = BasicOpponent(weak=True)
-    # elif self.episode < 2000:
-    else:
+    elif self.episode > self.curriculum["play vs weak"]:
         self.opponent = BasicOpponent(weak=False)
     return super().reset(one_starting, mode=0)
 
 
   def step(self, action):
-    if self.opponent is None:
-        return super().step(action)
     ob2 = self.obs_agent_two()
     a2 = self.opponent.act(ob2)
     action2 = np.hstack([action, a2])
