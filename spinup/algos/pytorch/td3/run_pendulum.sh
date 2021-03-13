457a5ecda5fe6aa80e16915f4fadf0b4248a857e
Julian Stastny <julianstastny@gmail.com>
2021-03-13 18:28:18 +0100

    ablation study on pendulum

---
+++ spinup/algos/pytorch/td3/run_pendulum.sh
@@ -0,0 +1,19 @@
+python3 td3.py --env_name CustomPendulum-v0 --hid 256 --l 2 --n 3 --psn 1 --decay 1 --layernorm 1 --epochs 100 --seed 0
+
+python3 td3.py --env_name CustomPendulum-v0 --hid 256 --l 2 --n 1 --psn 1 --decay 1 --layernorm 1 --epochs 100 --seed 0
+
+python3 td3.py --env_name CustomPendulum-v0 --hid 256 --l 2 --n 3 --psn 0 --decay 1 --layernorm 1 --epochs 100 --seed 0
+
+python3 td3.py --env_name CustomPendulum-v0 --hid 256 --l 2 --n 3 --psn 1 --decay 0 --layernorm 1 --epochs 100 --seed 0
+
+python3 td3.py --env_name CustomPendulum-v0 --hid 256 --l 2 --n 3 --psn 1 --decay 1 --layernorm 0 --epochs 100 --seed 0
+
+python3 td3.py --env_name CustomPendulum-v0 --hid 256 --l 2 --n 3 --psn 1 --decay 1 --layernorm 1 --epochs 100 --seed 1
+
+python3 td3.py --env_name CustomPendulum-v0 --hid 256 --l 2 --n 1 --psn 1 --decay 1 --layernorm 1 --epochs 100 --seed 1
+
+python3 td3.py --env_name CustomPendulum-v0 --hid 256 --l 2 --n 3 --psn 0 --decay 1 --layernorm 1 --epochs 100 --seed 1
+
+python3 td3.py --env_name CustomPendulum-v0 --hid 256 --l 2 --n 3 --psn 1 --decay 0 --layernorm 1 --epochs 100 --seed 1
+
+python3 td3.py --env_name CustomPendulum-v0 --hid 256 --l 2 --n 3 --psn 1 --decay 1 --layernorm 0 --epochs 100 --seed 1
