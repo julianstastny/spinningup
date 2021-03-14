e71a0aa144238ab2b7c4fc8d6eb1ac5d8d07bec2
Julian Stastny <julianstastny@gmail.com>
2021-03-14 14:37:29 +0100

    hockey ablation study

---
+++ spinup/algos/pytorch/td3/run_hockey_ablation.sh
@@ -0,0 +1,19 @@
+python3 td3.py --env HockeyCurriculum-v0 --hid 256 --l 2 --n 3 --psn 1 --decay 1 --layernorm 1 --epochs 200 --seed 0 --exp_name ablation --save_k_latest 5 --data_path /home/julianstastny/spiningup/data
+
+python3 td3.py --env HockeyCurriculum-v0 --hid 256 --l 2 --n 1 --psn 1 --decay 1 --layernorm 1 --epochs 200 --seed 0 --exp_name ablation --save_k_latest 5 --data_path /home/julianstastny/spiningup/data
+
+python3 td3.py --env HockeyCurriculum-v0 --hid 256 --l 2 --n 3 --psn 0 --decay 1 --layernorm 1 --epochs 200 --seed 0 --exp_name ablation --save_k_latest 5 --data_path /home/julianstastny/spiningup/data
+
+python3 td3.py --env HockeyCurriculum-v0 --hid 256 --l 2 --n 3 --psn 1 --decay 0 --layernorm 1 --epochs 200 --seed 0 --exp_name ablation --save_k_latest 5 --data_path /home/julianstastny/spiningup/data
+
+python3 td3.py --env HockeyCurriculum-v0 --hid 256 --l 2 --n 3 --psn 1 --decay 1 --layernorm 0 --epochs 200 --seed 0 --exp_name ablation --save_k_latest 5 --data_path /home/julianstastny/spiningup/data
+
+python3 td3.py --env HockeyCurriculum-v0 --hid 256 --l 2 --n 3 --psn 1 --decay 1 --layernorm 1 --epochs 200 --seed 1 --exp_name ablation --save_k_latest 5 --data_path /home/julianstastny/spiningup/data
+
+python3 td3.py --env HockeyCurriculum-v0 --hid 256 --l 2 --n 1 --psn 1 --decay 1 --layernorm 1 --epochs 200 --seed 1 --exp_name ablation --save_k_latest 5 --data_path /home/julianstastny/spiningup/data
+
+python3 td3.py --env HockeyCurriculum-v0 --hid 256 --l 2 --n 3 --psn 0 --decay 1 --layernorm 1 --epochs 200 --seed 1 --exp_name ablation --save_k_latest 5 --data_path /home/julianstastny/spiningup/data
+
+python3 td3.py --env HockeyCurriculum-v0 --hid 256 --l 2 --n 3 --psn 1 --decay 0 --layernorm 1 --epochs 200 --seed 1 --exp_name ablation --save_k_latest 5 --data_path /home/julianstastny/spiningup/data
+
+python3 td3.py --env HockeyCurriculum-v0 --hid 256 --l 2 --n 3 --psn 1 --decay 1 --layernorm 0 --epochs 200 --seed 1 --exp_name ablation --save_k_latest 5 --data_path /home/julianstastny/spiningup/data
